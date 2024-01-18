fc292e193515f4fcce92961b544925e08ac6dd88
BANG88 <sqibang@gmail.com>
2024-01-18 12:19:17 +0800

    Add main function and argument validation to name.py, and add cowsay module to say.py

--- name.py
+++ name.py
@@ -1,3 +1,21 @@
 import sys
 
-print("Hello, " + sys.argv[1] + "!")
+def main():
+	hello()
+
+	sys.exit(0)
+
+def hello():
+    if len(sys.argv) < 2:
+     print("Usage: python name.py <name>")
+
+    elif len(sys.argv) > 2:
+     print("Usage: python name.py <name>")
+
+    else:
+     print("Hello, " + sys.argv[1] + "!")
+
+for a in sys.argv[2:]:
+    print(a)
+
+main()
---
+++ say.py
@@ -0,0 +1,7 @@
+import cowsay
+import sys
+
+def main():
+	print(cowsay.tux(sys.argv[1]))
+
+main()
