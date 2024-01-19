3390cc054f009348f2edfacc57af62015d6356bc
BANG88 <sqibang@gmail.com>
2024-01-19 12:12:51 +0800

    Update students.csv and students.py to include city information

--- students.py
+++ students.py
@@ -1,14 +1,18 @@
+import csv
+
 students = []
 
 with open("students.csv") as file:
 	for line in file:
-		name, house = line.rstrip().split(",")
+		name, house, city = line.rstrip().split(",")
 		student = {
 			"name": name,
-			"house": house
+			"house": house,
+			"city": city
 		}
 		students.append(student)
 
 
-for student in students:
-	print(f"{student['name']} is in {student['house']}")
+for student in sorted(students, key=lambda student: student["name"]):
+	print(f"{student['name']} is in {student['house']} house and is from {student['city']}")
+
