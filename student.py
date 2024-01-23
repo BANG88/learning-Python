f4aad5c3f16182002369b05309b74d1c168f0696
BANG88 <sqibang@gmail.com>
2024-01-23 22:10:10 +0800

    Add date and hat classes, update student class

--- student.py
+++ student.py
@@ -7,15 +7,25 @@ class Student:
 		if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
 			raise ValueError("House is invalid")
 		self.name = name
-		self.house = house
+		self._house = house
 	
 	def __str__(self):
 		return f"Hello, my name is {self.name} from {self.house}!"
+	
+	@property
+	def house(self):
+		return self._house
+	
+	@house.setter
+	def house(self, house):
+		if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+			raise ValueError("House is invalid")
+		self.house = house
 
 def main():
 	student = get_student()
 	
-	print(f"Hello, {student.name} from {student.house}!")
+	print(student)
 
 def get_student():
   return Student(input("Name: "), input("House: "))
