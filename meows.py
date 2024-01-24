f1c301ff0f14496d3a0c26596cc53cda804681fe
BANG88 <sqibang@gmail.com>
2024-01-24 11:05:43 +0800

    cmd line

--- meows.py
+++ meows.py
@@ -1,22 +1,10 @@
-def meow(n:int) -> str:
-	"""
-	Meow n times
+import sys
 
-	:param n: number of meows
-	:type n: int
-	:rises: ValueError if n is negative
-	:returns: a string of n meows separated by newlines
-	:rtype: str
-	"""
-	return "meow \n" * n
-
-
-
-
-number:int  = int(input("How many times do you want to meow? "))
-
-meow(number)
-
-meows: str = meow(number)
-
-print(meows)
+if len(sys.argv) == 1:
+	print('meow')
+elif len(sys.argv) == 3 and sys.argv[1] == '-n':
+	n = int(sys.argv[2])
+	for i in range(n):
+		print('meow')
+else:
+	print('meow')
